ceeac27c6c5a56603c22a2ccf5cbe8a3f9154db7
Henrique Castro <henrique.monteiro.castro@outlook.com>
2022-09-12 14:11:34 -0300

    Final

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+
+
+const Fooder = () => {
+  return (
+    <footer className={styles.footer}>
+        <p>
+          <span>React + TS</span> @ 2022
+        </p>
+     </footer>
+  )
+}
+
+export default Fooder
